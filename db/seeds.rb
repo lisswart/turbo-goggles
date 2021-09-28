e5fc8ffcb2d4e367d286838229466752fa10cccc
Lisa Swart <arnoudlisa@gmail.com>
2021-09-27 21:53:26 -0400

    create subject matter model

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,13 +1,47 @@
-puts "seeding users..."
+# puts "seeding users..."
 
-5.times do 
-  User.create(
-    firstname: Faker::Name.first_name,
-    lastname: Faker::Name.last_name,
-    username: Faker::Twitter.unique.screen_name,
-    email: Faker::Internet.unique.email,
-    password_digest: Faker::Internet.unique.password
-  )
-end
+# 5.times do 
+#   User.create(
+#     firstname: Faker::Name.first_name,
+#     lastname: Faker::Name.last_name,
+#     username: Faker::Twitter.unique.screen_name,
+#     email: Faker::Internet.unique.email,
+#     password_digest: Faker::Internet.unique.password
+#   )
+# end
 
-puts "done seeding users 🌱"
+# puts "done seeding users 🌱"
+
+puts "seeding subject matters..."
+
+SubjectMatter.create(
+  title: "JavaScript"
+)
+SubjectMatter.create(
+  title: "Ruby"
+)
+SubjectMatter.create(
+  title: "Golang"
+)
+SubjectMatter.create(
+  title: "React"
+)
+SubjectMatter.create(
+  title: "Ruby on Rails"
+)
+SubjectMatter.create(
+  title: "Mathematics"
+)
+SubjectMatter.create(
+  title: "Data Structure and Algorithms"
+)
+SubjectMatter.create(
+  title: "English"
+)
+SubjectMatter.create(
+  title: "Communication"
+)SubjectMatter.create(
+  title: "Interview Skills"
+)
+
+puts "done seeding subject matters 🌱"
